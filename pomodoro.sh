0363cdda8bbb89c568a39d2deb46d5e7de22ce9e
Miha Dimec <miha.dimec@visionect.com>
2017-12-05 09:58:35 +0100

    more visible and longler lasting notifications

--- pomodoro.sh
+++ pomodoro.sh
@@ -18,7 +18,7 @@ echo "hit ^C to stop"
 while true
 do	
 	#desktop notification
-	notify-send --icon=gtk-info Test "Work"
+	notify-send -t 60000 --icon=gtk-info Work "Work\nStop goofing around and get to work... \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n :)"
 	#count work
 	for i in `seq 0 24`; #25 min
 	do
@@ -28,13 +28,13 @@ do
 	done    
 	
 	#desktop notification
-	notify-send --icon=gtk-info Test "Stand up and Relax"
+	notify-send -t 299000 --icon=gtk-info Relax "Stand up and Relax...\nStarch your legs, it's good for your health! \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n:D"
 	#play music
 	hour=`date +%H`
 	day=`date +%u` #1-7 1==Monday
-	if test "$hour" -ge 8 && test "$hour" -lt 19 && test "$day" -ne 6 && test "$day" -ne 7 #workdays(mon to fri) 8am to 7pm
+	if test "$hour" -ge 8 && test "$hour" -lt 17 && test "$day" -ne 6 && test "$day" -ne 7 #workdays(mon to fri) 8am to 7pm
 	then
-		mplayer -volume 50 "$mp3" 2>&1 > /dev/null &
+		mplayer -loop 0 -volume 58 "$mp3" 2>&1 > /dev/null & #68
 		echo  "Mplayer PID: " `jobs -p`
 	fi
 	#count pause
